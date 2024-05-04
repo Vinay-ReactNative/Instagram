3aea0deef4431d3766050cc33b330af1ee1e1b39
VinayKumarB <vinay@perisync.com>
2024-05-04 15:51:54 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { width } from './src/styles/styles'
+
+const App = () => {
+  return (
+    <View style={{ backgroundColor: 'red', flex: 1 }}>
+      <Text style={{ color: 'white' }}>Instagram clone</Text>
+    </View>
+  )
+}
+
+export default App
